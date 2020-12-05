4cbcd0e1dec57490e358cda5a6df7e960c737c0d
Arthur Pamart <pamarthur.aito@gmail.com>
2020-12-05 23:32:20 +0100

    Added a README

--- keyboards/40percentclub/gherkin/keymaps/liercms/keymap.c
+++ keyboards/40percentclub/gherkin/keymaps/liercms/keymap.c
@@ -1,4 +1,4 @@
-/* Copyright 2019 Stefano Marago'
+/* Copyright 2019 Arthur "Hedgestock" PAMART
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
