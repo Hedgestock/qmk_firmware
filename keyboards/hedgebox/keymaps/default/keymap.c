809e7ebfe92292a6ee7bd9a4c91a19504d28eb42
Hedgestock <pamarthur.aito@gmail.com>
2023-01-31 14:00:05 +0100

    Upgraded layout for strive

--- keyboards/hedgebox/keymaps/default/keymap.c
+++ keyboards/hedgebox/keymaps/default/keymap.c
@@ -1,15 +1,29 @@
 #include QMK_KEYBOARD_H
 
+#include "keymap_french.h"
+
 enum layers {
   KEY, // Keyboard inputs
+  KEY2,// Secondary keyboard inputs
+  ARR, // Keyboard inputs arrow based
   JOY, // Controller inputs
   FUN, // Fuction keys and layout switching
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [KEY] = LAYOUT_numpad_4x4(
-    KC_UP, KC_LEFT, KC_DOWN, KC_RIGHT,      KC_P, KC_K, KC_S, KC_D,
-                                            KC_A, KC_B, KC_C, KC_R,
+    FR_Q,    FR_S,    FR_D,     FR_F,       FR_U, FR_I, FR_O, FR_P,
+                                            FR_J, FR_K, FR_L, FR_M,
+                  KC_BSPC, LT(FUN, KC_DEL), KC_ENT,  KC_SPC
+    ),
+    [KEY2] = LAYOUT_numpad_4x4(
+    FR_A,    FR_Z,    FR_E,     FR_R,       FR_W, FR_X, FR_C, FR_V,
+                                            FR_T, FR_G, FR_Y, FR_H,
+                  KC_BSPC, LT(FUN, KC_DEL), KC_ENT,  KC_SPC
+    ),
+    [ARR] = LAYOUT_numpad_4x4(
+    KC_UP, KC_LEFT, KC_DOWN, KC_RIGHT,      FR_U, FR_I, FR_O, FR_P,
+                                            FR_J, FR_K, FR_L, FR_M,
                   KC_UP,  LT(FUN,KC_ENT), KC_SPC,  KC_H
     ),
     [JOY] = LAYOUT_numpad_4x4(
@@ -18,7 +32,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                       JS_BUTTON12,  LT(FUN,JS_BUTTON13), JS_BUTTON14,  JS_BUTTON15
     ),
     [FUN] = LAYOUT_numpad_4x4(
-    KC_NO, KC_NO, KC_NO, KC_NO,      TO(KEY), TO(JOY), KC_NO, KC_NO,
+    KC_NO, KC_NO, KC_NO, KC_NO,      TO(KEY), TO(KEY2), TO(ARR), TO(JOY),
                                      KC_NO, KC_NO, KC_NO, KC_NO,
                   KC_NO,  KC_NO, KC_NO,  KC_NO
     )
